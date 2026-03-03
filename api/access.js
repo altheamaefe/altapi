52c6f6bf5dca8d2bba774c926c21776de33485fe
altheamaefe <asagayno@accelevents.com>
2026-03-03 15:21:37 -0500

    Create access.js

---
+++ api/access.js
@@ -0,0 +1,15 @@
+export default function handler(req, res) {
+  const email = req.query.email;
+
+  if (email === "vip@test.com") {
+    return res.status(200).json({
+      allowed: true,
+      tier: "VIP"
+    });
+  }
+
+  return res.status(200).json({
+    allowed: false,
+    tier: "NONE"
+  });
+}
